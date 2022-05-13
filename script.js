0ca4fad57704d07a7f9b3b814b6722c9f7289730
Bruno Guimaraes <brunoguima13@gmail.com>
2022-05-13 14:31:53 -0300

    Adding menutoggle ~ done

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let show = true
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
-
 menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial"
